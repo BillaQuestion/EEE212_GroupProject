8cc1e5f720d2048cd88a878ca76d795dc24d5575
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2017-05-06 21:47:36 +0800

    init

---
+++ LaTeX/F1VTS29IVO5D4XS.ino
@@ -0,0 +1,30 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+#define Relay_Pin 4
+int Low_temp=3;
+int High_temp=7;
+ 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin();
+  pinMode(Relay_Pin, OUTPUT);
+  digitalWrite(Relay_Pin, LOW);
+}
+ 
+ 
+void loop(void)
+{
+  sensors.requestTemperatures(); 
+  Serial.print(sensors.getTempCByIndex(0));
+  if (sensors.getTempCByIndex(0) < Low_temp){
+    digitalWrite(Relay_Pin, HIGH);
+   }
+  if (sensors.getTempCByIndex(0) > High_temp){
+    digitalWrite(Relay_Pin, LOW);
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EEE212_GroupProject
+Group project of module EEE212.
