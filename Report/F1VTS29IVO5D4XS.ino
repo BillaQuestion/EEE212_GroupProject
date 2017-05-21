83295769da113481bbf3590d95ba0350d65ad6ea
Shiyao Zhang <zhangshiyao_zsy@outlook.com>
2017-05-21 21:36:12 +0800

    Add codes

---
+++ Report/F1VTS29IVO5D4XS.ino
@@ -0,0 +1,32 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+#define Relay_Pin 4
+int Low_temp=3;
+int High_temp=7;
+ 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin();
+  pinMode(Relay_Pin, OUTPUT);
+  digitalWrite(Relay_Pin, LOW);
+}
+ 
+ 
+void loop(void)
+{
+  sensors.requestTemperatures(); 
+  Serial.print(sensors.getTempCByIndex(0));
+  if (sensors.getTempCByIndex(0) < Low_temp){
+    /*Turn-off the relay module*/
+    digitalWrite(Relay_Pin, HIGH);
+   }
+  if (sensors.getTempCByIndex(0) > High_temp){
+    /*Turn-on the relay module*/
+    digitalWrite(Relay_Pin, LOW);
+  }
+}
